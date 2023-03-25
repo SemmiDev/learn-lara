511a35b851f41902f866dd5c1cb604cb0b12f1e0
Sammi Aldhi Yanto <sammidev4@gmail.com>
2023-03-25 13:58:51 +0700

    add

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite('resources/css/app.css')
+      @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.0/dist/trix.css">
